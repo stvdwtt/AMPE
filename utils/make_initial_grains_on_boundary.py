91eda5c60186f2ddf2972d5ae397ca9d18e32c23
stvdwtt <dewittsj@ornl.gov>
2020-10-09 16:02:36 -0400

    extended make_initial_grains_on_boundary to have sinusoidal perturbations

--- utils/make_initial_grains_on_boundary.py
+++ utils/make_initial_grains_on_boundary.py
@@ -37,9 +37,9 @@ parser.add_option( "-q", "--qlen", type="int", default=0,
                    help="number of component for q [default: %default]" )
 parser.add_option( "-c", "--nomconc", type="string",
                    help="nominal concentration" )
-parser.add_option( "--concentration-in", type="string", 
+parser.add_option( "--concentration-in", type="string",
                    help="concentration in interior region" )
-parser.add_option( "--concentration-out", type="string", 
+parser.add_option( "--concentration-out", type="string",
                    help="concentration in outside region" )
 parser.add_option( "--temperature", type="float",
                    help="temperature" )
@@ -49,10 +49,14 @@ parser.add_option( "--solid-fraction", type="float", default=1./60.,
                   help="solid-fraction at bottom")
 parser.add_option( "--noise", type="float", default=0.,
                   help="amplitude of noise in y interface")
-parser.add_option( "--double", action="store_true", dest="double_precision", 
+parser.add_option( "--double", action="store_true", dest="double_precision",
                   default=False)
 parser.add_option( "--plane", type="int", default=1,
                   help="direction orthogonal to plane")
+parser.add_option( "--sine_perturbation_amplitude", type="float", default=0.0,
+                help="nominal amplitude of the sinusoidal pertrubation to the interface")
+parser.add_option( "--sine_perturbation_cutoff", type="int", default=1,
+                help="largest number of periods across the domain for the sinusoidal perturbation")
 
 (options, args) = parser.parse_args()
 
@@ -77,7 +81,7 @@ dir2=2
 if plane==0:
   dir1=0
   dir0=1
-  
+
 
 ngrains = options.ngrains
 sf      = options.solid_fraction
@@ -103,6 +107,9 @@ if conc_inside is None :
 
 temperature = options.temperature
 
+sine_amplitude = options.sine_perturbation_amplitude
+sine_cutoff = options.sine_perturbation_cutoff
+
 # generate quaternions corresponding to random orientations
 random.seed( 112345 )
 quat_inside=[]
@@ -117,7 +124,7 @@ h3=h2
 def setRandomQinGrains():
   print("setRandomQinGrains...")
   if QLEN>0:
-    
+
     for g in range(ngrains):
 
       #pick a discrete angle between 0 and 2*pi
@@ -139,7 +146,7 @@ def setRandomQinGrains():
       else :
         if ( QLEN == 4 ) :
           #K. Shoemake. "Uniform random rotations."
-          #In D. Kirk, editor, Graphics Gems III, pages 124-132. Academic, New York, 1992. 
+          #In D. Kirk, editor, Graphics Gems III, pages 124-132. Academic, New York, 1992.
           u1 = random.uniform(0, 1.)
           u2 = random.uniform(0, 1.)
           u3 = random.uniform(0, 1.)
@@ -157,7 +164,7 @@ def setRandomQinGrains():
           x = math.sqrt(1.-u1)*math.cos(2.*math.pi*u2)
           y = math.sqrt(u1)*math.sin(2.*math.pi*u3)
           z = math.sqrt(u1)*math.cos(2.*math.pi*u3)
- 
+
         else : #QLEN=2
           w = math.cos( t )
           x = math.sin( t)
@@ -175,19 +182,21 @@ def setRandomQinGrains():
 #fill conc values
 nspecies=0
 if ( not ( nomconc is None ) ):
-  c = map( float, string.split( options.nomconc, ',' ) )
+  c = map( float, options.nomconc.split() )
   nspecies=len(c)
   print( "Nominal composition={}".format(c))
 if not(conc_inside is None):
-  ci = map( float, string.split( options.concentration_in, ',' ) )
+  #ci = map( float, string.split( options.concentration_in, ',' ) )
+  ci = map( float, options.concentration_in.split() )
+  print(list(ci))
   if nspecies==0:
-    nspecies=len(ci)
+    nspecies=len(list(ci))
   print( "Composition inside={}".format(ci))
 else:
   ci = N.zeros( nspecies, N.float32 )
 
 if ( not ( conc_outside is None ) ):
-  co = map( float, string.split( options.concentration_out, ',' ) )
+  co = map( float, options.concentration_out.split() )
   print( "Composition outside={}".format(co))
 else:
   co = N.zeros( nspecies, N.float32 )
@@ -262,7 +271,29 @@ else:
 # Fill data arrays
 xx = [0., 0., 0.]
 index = [0,0,0]
-fraction=1./ngrains; 
+fraction=1./ngrains;
+
+mode_amplitude = N.ones(sine_cutoff)
+mode_phase = N.zeros(sine_cutoff)
+
+for amplitude in N.nditer(mode_amplitude, op_flags=['writeonly']):
+    amplitude[...] = random.uniform(0.0,1.0)
+
+for phase_for_mode in N.nditer(mode_phase, op_flags=['writeonly']):
+    phase_for_mode[...] = random.uniform(0.0,2.0*math.pi)
+
+print(mode_amplitude)
+print(mode_phase)
+
+'''
+mode_amplitude[0] = 0.1
+mode_amplitude[1] = 0.3
+mode_amplitude[2] = 1.0
+mode_amplitude[3] = 0.5
+mode_amplitude[4] = 0.2
+mode_amplitude[5] = 1.0
+'''
+
 for j in range( nn[dir1] ) :
   #get a y in [0,1]
   xx[dir1] = (j + 0.5)/(1.*nn[dir1])
@@ -280,10 +311,18 @@ for j in range( nn[dir1] ) :
       index[dir0] = i
 
       d=d0+noise*random.uniform(-1., 1.)
-      
+
+
       if( widthy>0. ):
         if( d<0.1 ):
-          phase[index[2],index[1],index[0]] = 0.5*(1.+N.tanh(-3.*d/widthy))
+          perturbation = 0.;
+          for mode in range(1,sine_cutoff):
+            amplitude = 1.0/float(sine_cutoff)*mode_amplitude[mode]*sine_amplitude
+            temp = math.sin(2.0*math.pi * float(mode) * float(i)/float(nn[dir0]) + mode_phase[mode])
+            perturbation = perturbation + amplitude*temp
+
+          phase[index[2],index[1],index[0]] = 0.5*(1.+math.tanh(-3.*(d/widthy-perturbation)))
+
       else:
         if( d<0. ):
           phase[index[2],index[1],index[0]] = 1.
@@ -312,7 +351,7 @@ if QLEN>0:
       dx=abs(xx[0]-g*fraction*nn[dir0]-offset)
       if dx<0.5*fraction*nn[0]:
         gmin=g
-    
+
     print("Plane x={}, grain={}, q={}".format(xx,gmin,quat_inside[gmin]))
     for j in range( nn[1] ) :
       for k in range( nn[2] ) :
@@ -322,9 +361,11 @@ if QLEN>0:
         for m in range(QLEN):
           quat[m,k,j,i] = qi[m]
 
+ci_list = list(ci)
+co_list = list(co)
 for isp in range(nspecies):
   for x,y in N.nditer([conc[isp,:,:,:],phase], op_flags=['readwrite']):
-    x[...]= ci[isp]*y+co[isp]*(1.-y)
+      x[...]= ci_list[isp]*y+co_list[isp]*(1.-y)
 
 #-----------------------------------------------------------------------
 # Write data to file and close
@@ -340,5 +381,3 @@ for m in range(QLEN):
   ncquat[m][:,:,:]=quat[m,:,:,:]
 
 f.close()
-
-
